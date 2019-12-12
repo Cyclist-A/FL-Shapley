abf02dfd98670454e6d355d173368f108b101115
lishaofeng <shaofengli2013@gmail.com>
2019-12-12 21:22:57 +0800

    add the arguments parser to set up hyper parameters

--- run.py
+++ run.py
@@ -9,7 +9,25 @@ from net import MyNet
 from models.resnet import ResNet
 from federated import FederatedServer
 
-DATASET = 'cifa-10'
+import argparse
+
+parser = argparse.ArgumentParser(description='Federated Learning Framework')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--dataset', type=str, default='cifar-10', metavar='N',
+                    help='set up different datasets')
+parser.add_argument('--num-classes', type=int, default=10, metavar='N',
+                    help='output dimension size(default: 10)')
+parser.add_argument('--warm-up', action='store_true', default=False,
+                    help='enable warm up')
+parser.add_argument('--split-method', type=str, default='iid', metavar='N',
+                    help='split the dataset with different assumptions')
+parser.add_argument('--num-workers', type=int, default=3, metavar='N',
+                    help='the number of workers in FL(default: 3)')
+parser.add_argument('--num-rounds', type=int, default=1, metavar='N',
+                    help='the rounds of aggregation(default: 1)')
+
+
 
 # transformations
 TRAINSFORM_MINST = transforms.Compose([
@@ -29,30 +47,23 @@ TRAINSFORM_CIFA10_TEST = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-SERVER_SETTINGS = {
-    'warm_up': False,
-    'setting':{
-        'batch_size': 128
-    }
-}
-
 # CLIENT_SETTINGS = {}
 
-if __name__ == '__main__':
+def main(args):
     # some settings
     mp.set_start_method('spawn')
 
-    if DATASET == 'mnist':
+    if args.dataset == 'mnist':
         net = MyNet
         net_kwargs = None
         dataset = torchvision.datasets.MNIST
         transforms_train = TRAINSFORM_MINST
         transforms_test = TRAINSFORM_MINST
-    elif DATASET == 'cifa-10':
+    elif args.dataset == 'cifar-10':
         net = ResNet
         net_kwargs = {
             'depth': 32,
-            'num_classes': 10
+            'num_classes': args.num_classes
         }
         dataset = torchvision.datasets.CIFAR10
         transforms_train = TRAINSFORM_CIFA10_TRAIN
@@ -65,5 +76,15 @@ if __name__ == '__main__':
     DEVICE_LIST = ['cuda:' + str(i) for i in range(1, 4)]
 
     # run federated
-    fl = FederatedServer(net, trainset, testset, net_kwargs=net_kwargs, devices=DEVICE_LIST, split_method='iid', clients_num=3)
-    fl.run(rounds=1)
+    fl = FederatedServer(net, trainset, testset, net_kwargs=net_kwargs, devices=DEVICE_LIST, split_method=args.split_method, clients_num=args.num_workers)
+    fl.run(rounds=args.num_rounds)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    SERVER_SETTINGS = {
+        'warm_up': args.warm_up,
+        'setting': {
+            'batch_size': args.batch_size
+        }
+    }
+    main(args)
